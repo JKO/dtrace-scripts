b9e8f3c80c62fc3eaf3aa67fb458d9c2fb9b6277
Jacobo Tibaquira <jtibaquira@dragonjar.org>
2014-03-31 17:52:23 -0500

    Fixing Syntax

--- demos/tcpnmap2.d
+++ demos/tcpnmap2.d
@@ -1,14 +1,19 @@
 #!/usr/sbin/dtrace -s
+tcp:::accept-refused
+{
+ trace("Refused -- :)");
+ trace(args[2]->ip_daddr);trace(args[2]->tcps_lport);
+}
 
 tcp:::receive
-/args[4]->tcp_flags == 0/
+/args[4]->tcp_flags =!= 0/
 {
-  printf("TCP_null_scan ....\n")
+	trace("Flag 0");
 	trace(args[2]->ip_saddr);trace(args[3]->tcps_lport)
 }
 tcp:::receive
-/args[4]->tcp_flags == (TH_RST||TH_ECE||TH_CWR||TH_URG||TH_PUSH||TH_FIN||TH_SYN||TH_ACK)/
+/args[4]->tcp_flags == (TH_RST|TH_ECE|TH_CWR|TH_URG|TH_PUSH|TH_FIN|TH_SYN|TH_ACK)/
 {
-  printf("TCP_Xmas_scan ....\n")
+  trace("TYPE FLAGS");
   trace(args[2]->ip_saddr);trace(args[3]->tcps_lport)
 }
