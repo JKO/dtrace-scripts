1ea8e872bd053a8044d25765236c5fa37812df5d
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2014-03-31 18:40:48 -0500

    Fixing tcp flags

--- demos/tcpnmap2.d
+++ demos/tcpnmap2.d
@@ -5,13 +5,14 @@ tcp:::accept-refused
  trace(args[2]->ip_daddr);trace(args[4]->tcp_sport);
 }
 tcp:::receive
-/args[4]->tcp_flags != 0/
+/args[4]->tcp_flags == 0/
 {
 	trace("Flag 0");
 	trace(args[2]->ip_saddr);trace(args[3]->tcps_lport);trace(args[4]->tcp_flags);
 }
 tcp:::receive
-/args[4]->tcp_flags == (TH_RST|TH_ECE|TH_CWR|TH_URG|TH_PUSH|TH_FIN|TH_SYN|TH_ACK)/
+/args[4]->tcp_flags != 0/
+#(TH_RST|TH_ECE|TH_CWR|TH_URG|TH_PUSH|TH_FIN|TH_SYN|TH_ACK)
 {
   trace("TYPE FLAGS");
   trace(args[2]->ip_saddr);trace(args[3]->tcps_lport);trace(args[4]->tcp_flags);
