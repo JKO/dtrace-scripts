b642e886aa3598620b34630b7c97b506a7c6aab9
Jacobo Tibaquira <jtibaquira@dragonjar.org>
2014-03-31 17:07:31 -0500

    Fixing sysntax

--- demos/tcpnmap2.d
+++ demos/tcpnmap2.d
@@ -8,7 +8,7 @@ tcp:::receive
 /args[4]->tcp_flags == 0/
 {
   printf("TCP_null_scan ....\n")
-	trace(args[2]->ip_saddr);trace(args[3]->tcps_lport]);
+	trace(args[2]->ip_saddr);trace(args[3]->tcps_lport);
 }
 tcp:::receive
 /args[4]->tcp_flags == (TH_RST||TH_ECE||TH_CWR||TH_URG||TH_PUSH||TH_FIN||TH_SYN||TH_ACK)/
