952f8d45ad1bcc40cb218a4ac2d66e7f3782cfb3
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2014-03-31 18:53:51 -0500

    Testing Flags

--- demos/tcpnmap2.d
+++ demos/tcpnmap2.d
@@ -11,9 +11,17 @@ tcp:::receive
 	trace(args[2]->ip_saddr);trace(args[3]->tcps_lport);trace(args[4]->tcp_flags);
 }
 tcp:::receive
-/args[4]->tcp_flags != 0/
-#(TH_RST|TH_ECE|TH_CWR|TH_URG|TH_PUSH|TH_FIN|TH_SYN|TH_ACK)
-{
-  trace("TYPE FLAGS");
-  trace(args[2]->ip_saddr);trace(args[3]->tcps_lport);trace(args[4]->tcp_flags);
+/*/args[4]->tcp_flags != 0/
+*(TH_RST|TH_ECE|TH_CWR|TH_URG|TH_PUSH|TH_FIN|TH_SYN|TH_ACK)
+*/{
+    printf("%s", args[4]->tcp_flags & TH_FIN ? "FIN|" : "");
+    printf("%s", args[4]->tcp_flags & TH_SYN ? "SYN|" : "");
+    printf("%s", args[4]->tcp_flags & TH_RST ? "RST|" : "");
+    printf("%s", args[4]->tcp_flags & TH_PUSH ? "PUSH|" : "");
+    printf("%s", args[4]->tcp_flags & TH_ACK ? "ACK|" : "");
+    printf("%s", args[4]->tcp_flags & TH_URG ? "URG|" : "");
+    printf("%s", args[4]->tcp_flags & TH_ECE ? "ECE|" : "");
+    printf("%s", args[4]->tcp_flags & TH_CWR ? "CWR|" : "");
+    printf("%s", args[4]->tcp_flags == 0 ? "null " : "");
+    printf("\b)\n");
 }
