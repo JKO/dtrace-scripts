18de3f3f442865b6a5cb541d6f636ad6806b08c8
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2014-03-31 18:09:26 -0500

    change operator

--- demos/tcpnmap2.d
+++ demos/tcpnmap2.d
@@ -2,17 +2,17 @@
 tcp:::accept-refused
 {
  trace("Refused -- :)");
- trace(args[2]->ip_daddr);trace(args[2]->tcps_lport);
+ trace(args[2]->ip_daddr);trace(args[3]->tcps_lport);
 }
 tcp:::receive
 /args[4]->tcp_flags == 0/
 {
 	trace("Flag 0");
-	trace(args[2]->ip_saddr);trace(args[3]->tcps_lport)
+	trace(args[2]->ip_saddr);trace(args[3]->tcps_lport);trace(args[4]->tcp_flags);
 }
 tcp:::receive
 /args[4]->tcp_flags == (TH_RST|TH_ECE|TH_CWR|TH_URG|TH_PUSH|TH_FIN|TH_SYN|TH_ACK)/
 {
   trace("TYPE FLAGS");
-  trace(args[2]->ip_saddr);trace(args[3]->tcps_lport)
+  trace(args[2]->ip_saddr);trace(args[3]->tcps_lport);trace(args[4]->tcp_flags);
 }
