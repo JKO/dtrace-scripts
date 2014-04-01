e8d154c13ca82965bd367ba063663065a454139b
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2014-03-31 19:06:50 -0500

    testing output

--- demos/tcpnmap2.d
+++ demos/tcpnmap2.d
@@ -13,14 +13,14 @@ tcp:::receive
 tcp:::receive
 /args[4]->tcp_flags == (TH_RST|TH_ECE|TH_CWR|TH_URG|TH_PUSH|TH_FIN|TH_SYN|TH_ACK)/
 {
-    printf("%s %d %d", args[4]->tcp_flags & TH_FIN ? "FIN|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
-    printf("%s %d %d", args[4]->tcp_flags & TH_SYN ? "SYN|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
-    printf("%s %d %d", args[4]->tcp_flags & TH_RST ? "RST|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
-    printf("%s %d %d", args[4]->tcp_flags & TH_PUSH ? "PUSH|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
-    printf("%s %d %d", args[4]->tcp_flags & TH_ACK ? "ACK|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
-    printf("%s %d %d", args[4]->tcp_flags & TH_URG ? "URG|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
-    printf("%s %d %d", args[4]->tcp_flags & TH_ECE ? "ECE|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
-    printf("%s %d %d", args[4]->tcp_flags & TH_CWR ? "CWR|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
-    printf("%s %d %d", args[4]->tcp_flags == 0 ? "null " : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags & TH_FIN ? "FIN|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags & TH_SYN ? "SYN|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags & TH_RST ? "RST|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags & TH_PUSH ? "PUSH|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags & TH_ACK ? "ACK|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags & TH_URG ? "URG|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags & TH_ECE ? "ECE|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags & TH_CWR ? "CWR|" : "", args[3]->tcps_lport, args[2]->ip_saddr);
+    printf("%s %s %s", args[4]->tcp_flags == 0 ? "null " : "", args[3]->tcps_lport, args[2]->ip_saddr);
     printf("\b)\n");
 }
